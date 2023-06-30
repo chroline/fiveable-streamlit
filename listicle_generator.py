6e66b1452da997863f888b4fbbc4f05a18404bb4
Cole Gawin <colegawin@gmail.com>
2023-06-29 17:24:10 -0700

    allow emojis in headings

--- listicle_generator.py
+++ listicle_generator.py
@@ -11,23 +11,6 @@ class SessionState:
 
 openai.api_key = st.secrets['openai_api_key']
 
-EMOJI_PATTERN = re.compile(
-    "["
-    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    "\U0001F300-\U0001F5FF"  # symbols & pictographs
-    "\U0001F600-\U0001F64F"  # emoticons
-    "\U0001F680-\U0001F6FF"  # transport & map symbols
-    "\U0001F700-\U0001F77F"  # alchemical symbols
-    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-    "\U0001FA00-\U0001FA6F"  # Chess Symbols
-    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-    "\U00002702-\U000027B0"  # Dingbats
-    "\U000024C2-\U0001F251" 
-    "]+"
-)
-
 prompt = """
 Write an article for Fiveable titled "{article_title}". I will provide you with a list of sub-topics to include. Please write ~1 paragraph for each sub-topic, and make an H2 heading for each.
 
@@ -82,15 +65,6 @@ def main():
             try:
                 report.append(resp.choices[0].delta.content)
                 result = "".join(report).strip()
-
-                lines = result.split('\n')
-                for i, line in enumerate(lines):
-                    if line.startswith('#'):
-                        heading_without_emojis = EMOJI_PATTERN.sub('', line)
-                        lines[i] = heading_without_emojis.strip()
-
-                result = '\n'.join(lines)
-
                 res_box.markdown(f'{result}')
             except:
                 pass
