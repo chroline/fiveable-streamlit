88a34b6fb5a1cf38af24c1e87ab165ee66d93b47
Cole Gawin <colegawin@gmail.com>
2023-06-29 19:24:04 -0700

    update listicle generator

--- listicle_generator.py
+++ listicle_generator.py
@@ -1,12 +1,6 @@
-import re
-
 import openai
+import pandas as pd
 import streamlit as st
-
-
-class SessionState:
-    def __init__(self):
-        self.data = []
 
 
 openai.api_key = st.secrets['openai_api_key']
@@ -31,45 +25,90 @@ Paragraphs should cite specific information, not just generalizations.
 """
 
 st.title("Listicle Generator")
-st.subheader("Generate Fiveable listicle content using AI!")
+st.subheader("Generate Fiveable listicle content using ChatGPT")
 
+
+def generate_topics(df):
+    return "- " + "\n- ".join([f"{row['Term']}: {row['Definition']}" for index, row in df.iterrows()])
+
+
+def process_csv(file):
+    st.header("Output")
+
+    df = pd.read_csv(file, skiprows=2)
+    st.dataframe(df)
+
+    topics = None
+
+    col1, col2 = st.columns(2)
+    with col1:
+        categories = df["Category"].unique()
+        st.markdown("**Generate topics from category:**")
+        for value in categories:
+            if st.button(value):
+                topics = generate_topics(df[df['Category'] == value])
+    with col2:
+        key_topics = df.iloc[:, 0].astype(str).unique()
+        st.markdown("**Generate topics from key topic:**")
+        for value in key_topics:
+            if st.button(value):
+                topics = generate_topics(df[df.iloc[:, 0].astype(str) == value])
+
+    return topics
 
 def main():
-    # Text input for the title
-    article_title = st.text_input("Title", "")
+    # File upload widget
+    uploaded_file = st.file_uploader("Upload terms list file", type="csv")
 
-    # Text area input for the list
-    st.write("Enter a list of topics below, with each item on a new line:")
-    topics = st.text_area("Topics", "")
+    if 'topics' not in st.session_state:
+        st.session_state['topics'] = None
 
-    # Button to submit the list
-    if st.button("Submit"):
-        # Split the input by new lines and remove leading/trailing spaces
-        st.markdown("----")
-        res_box = st.empty()
+    if uploaded_file is not None:
+        # Process the uploaded file
+        new_topics = process_csv(uploaded_file)
+        st.session_state["topics"] = new_topics if new_topics is not None else st.session_state["topics"]
 
-        msg = prompt.format(article_title=article_title, topics=topics)
+    topics = st.session_state["topics"]
 
-        req = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                           messages=[
-                                               {"role": "system", "content": msg}
-                                           ],
-                                           max_tokens=2048,
-                                           temperature=1,
-                                           stream=True)
-
-        report = []
-        result = ""
-        for resp in req:
-            try:
-                report.append(resp.choices[0].delta.content)
-                result = "".join(report).strip()
-                res_box.markdown(f'{result}')
-            except:
-                pass
-
+    if topics is not None:
         st.divider()
-        st.text_area("Output", result)
+
+        topics_count = len(topics.split('\n'))
+        st.text_area(f"{topics_count} topics:", topics)
+
+        # Text input for the title
+        article_title = st.text_input("Title", "")
+
+        # Button to submit the list
+        if st.button("Generate"):
+            print("HEY")
+            # Split the input by new lines and remove leading/trailing spaces
+            st.markdown("----")
+            res_box = st.empty()
+
+            msg = prompt.format(article_title=article_title, topics=topics)
+
+            req = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                               messages=[
+                                                   {"role": "system", "content": msg}
+                                               ],
+                                               max_tokens=2048,
+                                               temperature=1,
+                                               stream=True)
+
+            report = []
+            result = ""
+            for resp in req:
+                print(resp)
+                try:
+                    report.append(resp.choices[0].delta.content)
+                    result = "".join(report).strip()
+                    res_box.markdown(f'{result}')
+                except:
+                    pass
+
+            st.divider()
+            st.text_area("Output", result)
 
 
 if __name__ == '__main__':
